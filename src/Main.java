470a258c9c99f8c9bf4b42c607ec3dcff901b8ce
AretanoGiuseppe <AretanoGiuseppe@INF02>
2022-04-27 12:19:48 +0200

    versione 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Aretano Giuseppe
+ * @author Caputo Daniel
+ * @author (solo firma) Scarlino Emanuele
+ * 
+ * @version 1
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
