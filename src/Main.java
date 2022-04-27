12ef4e2a943f24bc599afbbb46d58866ad08b614
Aretano <Aretano @INF02>
2022-04-27 12:25:44 +0200

    versione 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * 
  * @author Aretano Giuseppe
  * @author Caputo Daniel
- * @author (solo firma) Scarlino Emanuele
+ * @author Scarlino Emanuele
  * 
  * @version 1
  *
@@ -10,7 +10,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Aretano Giuseppe");
 		
 	}
 
